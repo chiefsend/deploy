23d62990cc8ea2daa8dfb38276a68dffeb82b9c6
Lukas <lukas.l.boehm@fau.de>
2021-07-26 20:04:09 +0200

    :sparkles: basic install script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # deploy
 Files and Documentation Neccecary to deploy ChiefSend
+
+# Requirements
+- nginx
+- redis-server
+
+# Install
+
+```
+sudo sh install.sh
+```
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export PORT=6969
+export DOMAIN=localhost
+export API_PATH=$(pwd)/../api
+export WEB_PATH=$(pwd)/../web
+
+# systemd api service
+envsubst < chiefsend.service > /etc/systemd/system/chiefsend.service
+systemctl daemon-reload
+
+# nginx config
+read -p "Use HTTPS (Y/n)?" choice
+case "$choice" in 
+  y|Y ) envsubst < chiefsend_https.conf > /etc/nginx/sites-available/chiefsend.conf;;
+  n|N ) envsubst < chiefsend_http.conf > /etc/nginx/sites-available/chiefsend.conf;;
+  * ) echo "invalid";;
+esac
+
+ln -f -s /etc/nginx/sites-available/chiefsend.conf /etc/nginx/sites-enabled/chiefsend.conf
+systemctl restart nginx
